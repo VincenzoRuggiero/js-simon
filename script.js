d822626730e979cfdf36965f3bdf804e270f29d2
VincenzoRuggiero <vincenzoru@live.it>
2022-12-02 17:27:13 +0100

    Push exercise

---
+++ script.js
@@ -0,0 +1,18 @@
+const buttonElem = document.getElementById("button");
+
+let points = 0;
+
+function clickCounter() {
+  buttonElem.addEventListener("click", function () {
+    while (points < 10) {
+      points++;
+      button.innerHTML = `${points}`;
+    }
+
+    if (points === 10) {
+      button.style.backgroundColor = "red";
+      button.setInterval(clickCounter, 5000);
+    }
+  });
+  return;
+}
