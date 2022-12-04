7104987fe2f214746d4a62a5d527337226a95fb0
VincenzoRuggiero <vincenzoru@live.it>
2022-12-04 09:43:29 +0100

    Exercise complete

--- Button Counter/script.js
+++ Button Counter/script.js
@@ -1,15 +1,21 @@
 const buttonElem = document.getElementById("button");
 
 let number = 0;
-let stop;
-buttonElem.addEventListener("click", function () {
-  add();
-  if (number > 10) {
-    let stop = setTimeout(add, 3000);
-  }
-});
 
 function add() {
-  number++;
-  buttonElem.innerHTML = `# ${number}`;
+  buttonElem.addEventListener("click", function () {
+    if (number < 10) {
+      number++;
+      buttonElem.innerHTML = `# ${number}`;
+    } else {
+      buttonElem.style.backgroundColor = "salmon";
+      setTimeout(() => {
+        number = 0;
+        buttonElem.style.backgroundColor = "";
+        buttonElem.innerHTML = `# ${number}`;
+      }, 3000);
+    }
+  });
 }
+
+add();
